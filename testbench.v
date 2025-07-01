ba634ed20b41ef4352f9cef2d6fc07a288ec5d17
Akash Chatterjee <chatterjeeakash54@gmail.com>
2025-07-01 22:40:33 +0530

    Add files via upload

--- testbench.v
+++ testbench.v
@@ -1,28 +1,22 @@
-module testbench();
+module testbench ();
 	reg [3:0] a;
 	reg [3:0] b;
 	reg carry_in;
 	wire [3:0] sum;
 	wire carry_out;
 	
-	integer i,j;
-	parameter LOOP_LIMIT = 8;
+	four_bit_ripplecarry_adder DUT (a,b,carry_in,sum,carry_out);
 	
-	fourbit_adder DUT (a,b,carry_in,sum,carry_out);
-	
-	initial begin 
-		for (i = 0; i < LOOP_LIMIT ; i = i+1) begin
-			for (j = 0; j < LOOP_LIMIT ; j = j + 1) begin
-				a = i; 
-				b = j; 
-				carry_in = i%2;
-				#10;
-				$display("a=%0d, b=%0d, carry_in=%0d, sum=%0d, carry_out=%0d", 
-				         a, b, carry_in, sum, carry_out);
-			end
-		end
-		
-		// End simulation
-		$finish;
+	initial begin
+		$monitor ("a = %0d,b =%0d,carry_in =%0d,sum = %0d,carry_out =%0d",a,b,carry_in,sum,carry_out);
 	end
+	
+	initial begin
+		#10; a = 4; b = 6;carry_in = 1;
+		#10; a = 4; b = 9;carry_in = 0;
+		#10; a = 7; b = 6;carry_in = 1;
+		#10; a = 3; b = 8;carry_in = 0;
+		#10; a = 6; b = 4;carry_in = 1;
+	end
+	
 endmodule
